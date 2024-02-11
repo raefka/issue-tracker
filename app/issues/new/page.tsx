074f1eb579d855487e372794985afc4f3c867e18
raefka <raefkacem20@gmail.com>
2024-02-11 20:36:20 +0100

    second commit

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+// Import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import 'easymde/dist/easymde.min.css';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import {z } from 'zod';
+
+
+type IssueForm = z.infer< typeof createIssueSchema>;
+
+// Use dynamic import for SimpleMDE
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false });
+
+const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit,formState :{ errors } } = useForm<IssueForm>({
+    resolver :zodResolver(createIssueSchema)
+  });
+  const [error , setError] = useState('');
+
+  return (
+    <div className='max-w-xl'>
+        {error && (
+                    <Callout.Root color='red' className='mb-5'>
+                    <Callout.Text>
+                    {error}
+                    </Callout.Text>
+                    </Callout.Root>)}
+    <form
+      className=' space-y-3'
+      onSubmit={handleSubmit(async (data) => {
+        try {
+            await axios.post('/api/issues', data);
+            router.push('/issues');
+        } catch (error) {
+            setError('An unexpected error occurred .')
+        }
+       
+      })}
+    >
+      <TextField.Root>
+        <TextField.Input placeholder='title' {...register('title')} />
+      </TextField.Root>
+      {errors.title && <Text color='red' as="p">{errors.title.message}</Text>}
+      <Controller
+        name='description'
+        control={control}
+        render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
+      />
+      {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+      <Button>Submit New Issue</Button>
+    </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
+import '@radix-ui/themes/styles.css';
+import'./theme-config.css';
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Theme, ThemePanel} from '@radix-ui/themes'
+import NavBar from './NavBar'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'],
+variable: '--font-inter' })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +21,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.variable}>
+        
+        < Theme accentColor="iris">
+        <NavBar />
+        <main className='p-5'>{children}</main>
+        </Theme>
+        </body>
     </html>
   )
 }
